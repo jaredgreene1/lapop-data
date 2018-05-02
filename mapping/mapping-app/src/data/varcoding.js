40edac25c8804165e8c3dde74bc1d6aa8c37b105
Jared <jaredgreene1@gmail.com>
2018-05-01 19:24:59 -0600

    Add importable variable info to be used across the app

---
+++ mapping/mapping-app/src/data/varcoding.js
@@ -0,0 +1,30 @@
+const GT_2014 = {
+  'townhall attendance': 'NP1',
+  'protest attendance': 'Prot3',
+  'interest in politics': 'POL1',
+  'town': 'MUNICIPIO'
+}
+
+export const codes17 = {
+  prot3: {
+    code: 'prot3',
+    label:'protest attendance',
+    text: "In the last 12 months, have you participated in a public demonstration or protest?",
+  },
+  pol1: {
+    code: 'pol1',
+    label: 'interest in politics',
+    text: 'How much interest do you have in politics?',
+  },
+  www1: {
+    code: 'www1',
+    label: 'internet use',
+    text: 'How frequently do you use the internet?',
+  },
+  indig: {
+    code: 'indig',
+    label: 'indigenous identity',
+    text: 'Are you indigenous?'
+  },
+}
+
