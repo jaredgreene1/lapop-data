6f5bfeabe8461712b09216028d827d46cab46b3c
Jared <jaredgreene1@gmail.com>
2018-05-02 10:26:53 -0600

    Clean file organization and delete duplicate maps and data files

--- mapping/mapping-app/src/data/data.js
+++ mapping/mapping-app/src/data/data.js
@@ -1,39 +1,30 @@
 import deptData from './data_0.json';
 import muniData from './data_1.json';
 
+import { codes17 } from './varcoding'
                                                                            
 const getDeptData = (geoData) => {                                         
   const idx = Object.values(deptData['prov']).indexOf(geoData.NAME_1)   
-  return {
-    pol1: deptData['pol1'][idx],
-    //ww1: deptData['ww1'][idx],
-    indig: deptData['indig'][idx]
-  }
-}                                                                      
-
+  return Object.keys(codes17).reduce((data, key) => { 
+      data[key] = deptData[key][idx] 
+      return data}, {})
+}
 
 const getMuniData = (geoData) => {
 	const locale = ( geoData.NAME_2 + ', ' + geoData.NAME_1 )               
 	const idx = Object.values(muniData['municipio']).indexOf(locale)
-  return {
-    pol1: muniData['pol1'][idx],
-    www1: muniData['www1'][idx],
-    indig: muniData['indig'][idx]
-  }
-
+  return Object.keys(codes17).reduce((data, key) => { 
+      data[key] = muniData[key][idx] 
+      return data}, {})
 }
 
 const getData = (geoData) => {
-  console.log(geoData)
-  console.log(geoData.NAME_2)
   if(geoData.NAME_2) {
-    console.log("MUNI")
     return getMuniData(geoData)
   } else {
-    //console.log("DEPT")
     return getDeptData(geoData)
   }
 }
 
-export default getData
+export default getData 
 
