f9dfd90a4f0e61cd1874d42bdd29a90a0dc173fa
Jared <jaredgreene1@gmail.com>
2018-05-03 14:05:07 -0600

    Fix naming in data.js and add new background

--- mapping/mapping-app/src/data/data.js
+++ mapping/mapping-app/src/data/data.js
@@ -3,14 +3,14 @@ import muniData from './data_1.json';
 
 import { codes17 } from './varcoding'
                                                                            
-const getDeptData = (geoData) => {                                         
+const getDeptDataByLocation = (geoData) => {                                         
   const idx = Object.values(deptData['prov']).indexOf(geoData.NAME_1)   
   return Object.keys(codes17).reduce((data, key) => { 
       data[key] = deptData[key][idx] 
       return data}, {})
 }
 
-const getMuniData = (geoData) => {
+const getMuniDataByLocation = (geoData) => {
 	const locale = ( geoData.NAME_2 + ', ' + geoData.NAME_1 )               
 	const idx = Object.values(muniData['municipio']).indexOf(locale)
   return Object.keys(codes17).reduce((data, key) => { 
@@ -18,13 +18,23 @@ const getMuniData = (geoData) => {
       return data}, {})
 }
 
-const getData = (geoData) => {
+export const getDataByLocation = (geoData) => {
   if(geoData.NAME_2) {
-    return getMuniData(geoData)
+    return getMuniDataByLocation(geoData)
   } else {
-    return getDeptData(geoData)
+    return getDeptDataByLocation(geoData)
   }
 }
 
-export default getData 
-
+export const getData = (grouping) => {
+  switch(grouping) {
+    case 'municipio':
+      return muniData;
+      break;
+    case 'departamento':
+      return deptData
+      break;
+    default:
+      throw "Invalid data grouping: " + grouping
+  }
+}
