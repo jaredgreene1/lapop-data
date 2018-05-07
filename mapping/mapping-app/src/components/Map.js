dfa181153fad3f438bb93b97d1ec0b7afe526147
Jared <jaredgreene1@gmail.com>
2018-05-06 23:01:50 -0600

    Map maps slightly more responsive and fix zoom buttons

--- mapping/mapping-app/src/components/Map.js
+++ mapping/mapping-app/src/components/Map.js
@@ -16,6 +16,25 @@ const LOWCOLOR  = '#005eb1'
 const MIDCOLOR  = '#2769d4'
 const HIGHCOLOR = '#F3F9FE'
 
+const zoomButtons = {
+  display: 'flex',
+  flexDirection: 'column',
+  left: '93%',
+  top: '81%',
+  width: 'fit-content',
+  position: 'relative',
+  boxShadow: 'black 1px 1px 3px',
+  borderRadius: '2px',
+  padding: '3px',
+  background: '#fbfafa',
+}
+
+const button = {
+  background: 'none',
+  border: 'none',
+  padding: '2px',
+
+}
 
 export class Map extends Component {
   constructor(props) {
@@ -26,6 +45,8 @@ export class Map extends Component {
       muni: '',
       data: null,
       forceUpdate: false,  
+      width: 200,
+      height: 200,
     }
   }
 
@@ -45,12 +66,15 @@ export class Map extends Component {
     ]).range([HIGHCOLOR, MIDCOLOR, LOWCOLOR])
 
   componentDidUpdate = (prevProps, prevState, snapshot) => {
+    this.resize()
     if(this.state.forceUpdate){
       this.setState({
         forceUpdate: false
       })
     }
   }
+
+  componentDidMount = () => this.resize()
 
   handleZoomIn = () => {
     this.setState({ zoom: this.state.zoom * 1.15, })
@@ -77,6 +101,16 @@ export class Map extends Component {
    wheel.preventDefault()
   }
 
+  resize = () => {
+   let el = document.getElementById('chart-output')
+    if(el && (Math.abs(el.offsetHeight-this.state.height) > 10 || el.offsetWidth !== this.state.width))
+    {
+      this.setState({
+        height: el.offsetHeight,
+        width: el.offsetWidth
+      })
+    }
+  }
 
   render() {
     if(this.props.disabled)
@@ -88,14 +122,25 @@ export class Map extends Component {
         </div>)
     else
       return(
-        <div>
-          <h3> {this.props.variable.label} </h3>
+        <div style={{display:'flex', flexDirection:'column', height:'100%'}}>
+          <h3 style={{margin:'0'}}> {this.props.variable.label} </h3>
           <hr />
-          <div style={{padding: '10px', display: 'flex'}}> 
-            <Button active={true} bsSize={'small'} onClick={ this.handleZoomIn }> { '+' } </Button> 
-              <Button bsSize={'lg'} onClick={ this.handleZoomOut }> { '-' } </Button> 
-            </div>
-            <div>  
+          <div style={zoomButtons}> 
+            <button 
+              style={Object.assign({}, button, 
+                        {
+                          borderBottom: '1px solid #e6e6e6',
+                          padding: '4px',
+                        }
+                        )
+                    } 
+              onClick={ this.handleZoomIn }> 
+              { '+' } 
+            </button> 
+
+            <button style={ button } onClick={ this.handleZoomOut }> { '-' } </button> 
+          </div>
+            <div style={{display: 'flex', flexGrow: '2', height:'100%'}}>  
               {this.state.dept ? 
                 <InfoBox 
                   data={ this.state.data }
@@ -103,45 +148,45 @@ export class Map extends Component {
                   municipality={ this.state.muni}/> 
                 : null
               }
-              <ComposableMap width={500} height={500} projectionConfig={{
-                  scale: 6000,
-                  xOffset: 3506,
-                  yOffset: 710,
-              }}>
-                <ZoomableGroup zoom={ this.state.zoom }>
-                <Geographies 
-                  geography={ this.props.geoData } 
-                  disableOptimization={ this.state.forceUpdate }
-                > 
-                  {(geographies, projection) => geographies.map((geography, i) => {
-                  geography.properties['data'] = getDataByLocation(geography.properties, this.props.data) 
-                  return(
-                    <Geography
-                      key={ geography.id }
-                      geography={ geography }
-                      cacheId={ 'geography-' +  geography.properties.ID_2 + geography.properties.ID_1 + this.props.geoData[0]}
-                      projection={ projection }
-                      onClick={ this.handleClick }
-                      onMouseOver={ (e) => this.handleMouseOver(e, geography.properties)}
-                      onWheel={ this.handleWheel }
-                      style = {{
-                        default: { 
-                          fill: this.colorScale()(geography.properties.data[this.props.variable.code]),
-                          stroke: "#000",
-                          strokeWidth: "0.2",
-                          outline: "none",
-                        },
-                        hover:   { 
-                          fill: "#999",
-                          boxShadow: '10px 3px red',
-                        },
-                      }}
-                    />
+              <div id='chart-output' style={{width: '100%', height: '100%'}}>
+                <ComposableMap width={this.state.width} height={this.state.height} projectionConfig={{
+                    scale: 6000,
+                }}>
+                  <ZoomableGroup zoom={ this.state.zoom } center={[-92, 17]}>
+                  <Geographies 
+                    geography={ this.props.geoData } 
+                    disableOptimization={ this.state.forceUpdate }
+                  > 
+                    {(geographies, projection) => geographies.map((geography, i) => {
+                    geography.properties['data'] = getDataByLocation(geography.properties, this.props.data) 
+                    return(
+                      <Geography
+                        key={ geography.id }
+                        geography={ geography }
+                        cacheId={ 'geography-' +  geography.properties.ID_2 + geography.properties.ID_1 + this.props.geoData[0]}
+                        projection={ projection }
+                        onClick={ this.handleClick }
+                        onMouseOver={ (e) => this.handleMouseOver(e, geography.properties)}
+                        onWheel={ this.handleWheel }
+                        style = {{
+                          default: { 
+                            fill: this.colorScale()(geography.properties.data[this.props.variable.code]),
+                            stroke: "#000",
+                            strokeWidth: "0.2",
+                            outline: "none",
+                          },
+                          hover:   { 
+                            fill: "#999",
+                            boxShadow: '10px 3px red',
+                          },
+                        }}
+                      />
+                    )}
                   )}
-                )}
-              </Geographies>
-              </ZoomableGroup>
-            </ComposableMap>
+                </Geographies>
+                </ZoomableGroup>
+              </ComposableMap>
+            </div>
           </div>
         </div>
       )
